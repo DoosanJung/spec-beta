97a42041e2f707315f7e4f0559f3c0e94534f31a
DoosanJung <dootaa00@gmail.com>
2018-01-24 03:31:20 +0900

    __init__

---
+++ spec-beta/SpecBeta.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+'''
+'''
+
+class SpecBeta(object):
+
+    def run(self):
+        print 'running'
+
+
+if __name__=='__main__':
+    specBeta = SpecBeta()
+    specBeta.run()
