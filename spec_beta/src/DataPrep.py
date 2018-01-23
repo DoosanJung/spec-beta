c52f250bbf5467bce9ee6df8cecb2b011a726e07
DoosanJung <dootaa00@gmail.com>
2018-01-24 04:15:04 +0900

    __init__

---
+++ spec_beta/src/DataPrep.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+'''
+'''
+
+import logging
+from spec_beta.conf.SpecBetaConfig import SpecBetaConfig
+
+logging.config.fileConfig("miner2/conf/MinerLog.cfg")
+logger = logging.getLogger()
+
+
+class DataPrep(object):
+    pass
